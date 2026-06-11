7f6ec6a544316557ddb1c8222fb29778396e428a
zzmeyuka <uldanarahmetova@gmail.com>
2026-06-12 00:48:23 +0500

    Update iOS api url to ngrok

--- mobile/lib/core/constants/api_config.dart
+++ mobile/lib/core/constants/api_config.dart
@@ -5,7 +5,7 @@ class ApiConfig {
   static String get baseUrl {
     if (kIsWeb) return 'http://localhost:3000/api';
     if (Platform.isAndroid) return 'https://blemish-vividness-unsuited.ngrok-free.dev/api';
-    return 'http://localhost:3000/api';
+    return 'https://blemish-vividness-unsuited.ngrok-free.dev/api';
   }
 }
 
